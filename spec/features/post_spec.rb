7f8790bf374499c9b3aa43c24c521f229423ae6d
neil-shuntyard <neil@shuntyard.com>
2018-01-28 20:43:17 +0200

    updated seed file and posts index view

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,21 +1,31 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(user, :scope => :user)
+  end
+
   describe 'homepage' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Posts1")
+      post2 = Post.create(date: Date.today, rationale: "Posts2")
+      visit posts_path
+      expect(page).to have_content(/Posts1|Posts2/)
     end
   end
 
   describe 'creation' do
     before do
-      user = User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
